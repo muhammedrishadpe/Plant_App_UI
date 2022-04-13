93dd90ff36a258af1bbff7fc154bd6b573178bfb
muhammedrishadpe <rishadpe@gmail.com>
2022-04-13 16:37:35 +0530

    Title with more btn

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:plantapp/constants.dart';
+import 'package:plantapp/screens/home/components/title_with_more_btn.dart';
 import '../components/header_with_searchbox.dart';
 
 class Body extends StatelessWidget {
@@ -16,55 +19,114 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size: size),
-          Row(
-            children: [
-              TitleWithCustomUnderline(
-                text: "Recomended",
-              ),
-              FlatButton(
-                onPressed: () {},
-                child: Text("More"),
-              ),
-            ],
-          )
+          TitleWithMoreBtn(
+            title: "Recomended",
+            press: () {},
+          ),
+          // it WIll Cover 40% of our total width
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                RecomendedPlantCard(
+                  image: "assets/images/image_1.png",
+                  title: "Samantha",
+                  country: "Russia",
+                  press: () {},
+                  price: 12,
+                ),
+                RecomendedPlantCard(
+                  image: "assets/images/image_1.png",
+                  title: "Samantha",
+                  country: "Russia",
+                  press: () {},
+                  price: 12,
+                ),
+                RecomendedPlantCard(
+                  image: "assets/images/image_1.png",
+                  title: "Samantha",
+                  country: "Russia",
+                  press: () {},
+                  price: 12,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 }
 
-class TitleWithCustomUnderline extends StatelessWidget {
-  const TitleWithCustomUnderline({
+class RecomendedPlantCard extends StatelessWidget {
+  const RecomendedPlantCard({
     Key? key,
-    required this.text,
+    required this.image,
+    required this.title,
+    required this.country,
+    required this.price,
+    required this.press,
   }) : super(key: key);
-
-  final String text;
-
+  final String image, title, country;
+  final int price;
+  final Function()? press;
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Container(
-      height: 24,
-      child: Stack(
+      margin: EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding * 2.5),
+      width: size.width * 0.4,
+      child: Column(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
-            child: Text(
-              text,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
+          Image.asset(image),
+          GestureDetector(
+            onTap: press,
             child: Container(
-              margin: EdgeInsets.only(right: kDefaultPadding / 4),
-              height: 7,
-              color: kPrimaryColor.withOpacity(0.2),
+              padding: EdgeInsets.all(kDefaultPadding / 2),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withOpacity(0.23),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "$title\n".toUpperCase(),
+                          style: Theme.of(context).textTheme.button,
+                        ),
+                        TextSpan(
+                          text: "$country".toUpperCase(),
+                          style:
+                              TextStyle(color: kPrimaryColor.withOpacity(0.5)),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    '\$$price',
+                    style: Theme.of(context)
+                        .textTheme
+                        .button
+                        ?.copyWith(color: kPrimaryColor),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
